58fdf0166f06ce45f56cb8e8575d721f914b2ae8
Jens Meindertsma <mail@jensmeindertsma.com>
2022-12-09 11:15:58 +0100

    Day 02 part A

--- years/2022/days/02/src/bin/part_a.rs
+++ years/2022/days/02/src/bin/part_a.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{cmp::Ordering, fs, str::FromStr};
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
@@ -8,8 +8,67 @@ fn main() {
     println!("Total score: {}", score);
 }
 
-fn calculate_score(_input: &str) -> u32 {
-    todo!()
+fn calculate_score(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut sides = line.split(' ');
+            let opponent = sides.next().unwrap().parse::<Move>().unwrap();
+            let suggested = sides.next().unwrap().parse::<Move>().unwrap();
+
+            let outcome_score = match suggested.partial_cmp(&opponent) {
+                Some(Ordering::Less) => 0,
+                Some(Ordering::Equal) => 3,
+                Some(Ordering::Greater) => 6,
+                None => unreachable!(),
+            };
+
+            let shape_score = suggested as u8;
+
+            (shape_score + outcome_score) as u32
+        })
+        .sum()
+}
+
+#[derive(PartialEq, Debug)]
+enum Move {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+impl FromStr for Move {
+    type Err = &'static str;
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        match string {
+            "A" | "X" => Ok(Move::Rock),
+            "B" | "Y" => Ok(Move::Paper),
+            "C" | "Z" => Ok(Move::Scissors),
+            _ => Err("Unknown move"),
+        }
+    }
+}
+
+impl PartialOrd for Move {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(match self {
+            Move::Rock => match other {
+                Move::Rock => Ordering::Equal,
+                Move::Paper => Ordering::Less,
+                Move::Scissors => Ordering::Greater,
+            },
+            Move::Paper => match other {
+                Move::Rock => Ordering::Greater,
+                Move::Paper => Ordering::Equal,
+                Move::Scissors => Ordering::Less,
+            },
+            Move::Scissors => match other {
+                Move::Rock => Ordering::Less,
+                Move::Paper => Ordering::Greater,
+                Move::Scissors => Ordering::Equal,
+            },
+        })
+    }
 }
 
 #[cfg(test)]
