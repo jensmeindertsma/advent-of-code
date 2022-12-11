dbed7a43cb68b8e1ca9c6a7d35b8d811aca99d4e
Jens Meindertsma <mail@jensmeindertsma.com>
2022-12-11 18:29:14 +0100

    Complete part A

--- years/2022/days/06/src/bin/part_a.rs
+++ years/2022/days/06/src/bin/part_a.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
@@ -9,17 +9,52 @@ fn main() {
 }
 
 fn count_processed(input: &str) -> usize {
-    todo!("{input}");
+    let characters: Vec<char> = input.chars().collect();
+
+    let (first_index, _) = characters
+        .windows(4)
+        .enumerate()
+        .find(|(_, window)| {
+            let mut map = HashSet::new();
+
+            // `map.insert` returns `false` for duplicates, which we then negate to true.
+            let contains_duplicates = window.iter().any(|char| !map.insert(*char));
+
+            !contains_duplicates
+        })
+        .unwrap();
+
+    first_index + 4
 }
 
 #[cfg(test)]
 mod tests {
-    use day_06::TEST_INPUT;
+    use day_06::*;
 
     use super::count_processed;
 
     #[test]
-    fn example_list() {
-        assert_eq!(count_processed(TEST_INPUT), 7)
+    fn test_input_a() {
+        assert_eq!(count_processed(TEST_INPUT_A), 7)
+    }
+
+    #[test]
+    fn test_input_b() {
+        assert_eq!(count_processed(TEST_INPUT_B), 5)
+    }
+
+    #[test]
+    fn test_input_c() {
+        assert_eq!(count_processed(TEST_INPUT_C), 6)
+    }
+
+    #[test]
+    fn test_input_d() {
+        assert_eq!(count_processed(TEST_INPUT_D), 10)
+    }
+
+    #[test]
+    fn test_input_e() {
+        assert_eq!(count_processed(TEST_INPUT_E), 11)
     }
 }
--- years/2022/days/06/src/lib.rs
+++ years/2022/days/06/src/lib.rs
@@ -1,1 +1,9 @@
-pub const TEST_INPUT: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+pub const TEST_INPUT_A: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+pub const TEST_INPUT_B: &str = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+
+pub const TEST_INPUT_C: &str = "nppdvjthqldpwncqszvftbrmjlhg";
+
+pub const TEST_INPUT_D: &str = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+
+pub const TEST_INPUT_E: &str = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
