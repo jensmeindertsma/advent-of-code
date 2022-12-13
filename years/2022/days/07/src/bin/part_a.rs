fc79647066bf6fed2639b7990268a5b8263afaca
Jens Meindertsma <mail@jensmeindertsma.com>
2022-12-13 18:01:54 +0100

    Parsing for part A

--- years/2022/days/07/src/bin/part_a.rs
+++ years/2022/days/07/src/bin/part_a.rs
@@ -9,7 +9,78 @@ fn main() {
 }
 
 fn calculate_size(input: &str) -> usize {
-    todo!("{input}");
+    let mut lines = input.lines().peekable();
+    let mut operations: Vec<Operation> = Vec::new();
+
+    while let Some(line) = lines.next() {
+        if !line.starts_with('$') {
+            panic!("A line following `ls` wasn't properly parsed.")
+        }
+
+        // Skip the `$`.
+        let mut slices = line.split_whitespace().skip(1);
+
+        let operation = match slices.next().unwrap() {
+            "cd" => {
+                let direction = match slices.next().unwrap() {
+                    "/" => Direction::Root,
+                    ".." => Direction::Up,
+                    name => Direction::Down { name },
+                };
+
+                Operation::Move(direction)
+            }
+            "ls" => {
+                let mut entries = Vec::new();
+
+                // Collect up all the following lines until the next command.
+                // These lines form the output of the `ls` command.
+                while let Some(line) = lines.next_if(|line| !line.starts_with('$')) {
+                    let mut slices = line.split_whitespace();
+
+                    let entry = match slices.next().unwrap() {
+                        "dir" => Entry::Directory {
+                            name: slices.next().unwrap(),
+                        },
+                        other => Entry::File {
+                            size: other.parse().unwrap(),
+                            name: slices.next().unwrap(),
+                        },
+                    };
+
+                    entries.push(entry);
+                }
+
+                Operation::List(entries)
+            }
+            other => panic!("Unexpected command: {other}"),
+        };
+
+        operations.push(operation);
+    }
+
+    println!("{operations:#?}");
+
+    0
+}
+
+#[derive(Debug)]
+enum Operation<'a> {
+    Move(Direction<'a>),
+    List(Vec<Entry<'a>>),
+}
+
+#[derive(Debug)]
+enum Direction<'a> {
+    Root,
+    Up,
+    Down { name: &'a str },
+}
+
+#[derive(Debug)]
+enum Entry<'a> {
+    File { size: u32, name: &'a str },
+    Directory { name: &'a str },
 }
 
 #[cfg(test)]
