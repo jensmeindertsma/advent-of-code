95f8ccaee460a69b18de36078f2a05fdc0c3c158
Jens Meindertsma <mail@jensmeindertsma.com>
2022-12-19 14:12:44 +0100

    Complete day 10 part A

--- years/2022/days/10/src/bin/part_a.rs
+++ years/2022/days/10/src/bin/part_a.rs
@@ -3,13 +3,95 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
 
-    let sum = measure_signal_strength(&input);
+    let total = measure_signal_strength(&input);
 
-    println!("Sum of signal strength measurements: {sum}");
+    println!("Total signal strength: {total}");
 }
 
-fn measure_signal_strength(input: &str) -> usize {
-    todo!("{input}");
+fn measure_signal_strength(input: &str) -> i32 {
+    let instructions: Vec<Instruction> = input
+        .lines()
+        .map(|line| {
+            let mut slices = line.split_whitespace();
+
+            match slices.next().unwrap() {
+                "noop" => Instruction::Sleep,
+                "addx" => Instruction::Add(slices.next().unwrap().parse().unwrap()),
+                other => panic!("Unexpected instruction: `{other}`"),
+            }
+        })
+        .collect();
+
+    let mut computer = Computer::new();
+
+    for instruction in instructions {
+        computer.execute(instruction);
+    }
+
+    computer
+        .terminate()
+        .iter()
+        .filter(|snapshot| [20, 60, 100, 140, 180, 220].contains(&snapshot.cycle))
+        .map(|snapshot| snapshot.cycle as i32 * snapshot.value)
+        .sum()
+}
+
+#[derive(Debug)]
+enum Instruction {
+    Sleep,
+    Add(i32),
+}
+
+struct Computer {
+    current_cycle: u16,
+    current_value: i32,
+    snapshots: Vec<Snapshot>,
+}
+
+impl Computer {
+    fn new() -> Self {
+        Self {
+            current_cycle: 0,
+            current_value: 1,
+            snapshots: Vec::new(),
+        }
+    }
+
+    fn execute(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::Sleep => {
+                self.complete_cycle();
+                self.snapshot();
+            }
+            Instruction::Add(amount) => {
+                self.complete_cycle();
+                self.snapshot();
+                self.complete_cycle();
+                self.snapshot();
+                self.current_value += amount;
+            }
+        }
+    }
+
+    fn complete_cycle(&mut self) {
+        self.current_cycle += 1
+    }
+
+    fn snapshot(&mut self) {
+        self.snapshots.push(Snapshot {
+            cycle: self.current_cycle,
+            value: self.current_value,
+        })
+    }
+
+    fn terminate(self) -> Vec<Snapshot> {
+        self.snapshots
+    }
+}
+
+struct Snapshot {
+    cycle: u16,
+    value: i32,
 }
 
 #[cfg(test)]
