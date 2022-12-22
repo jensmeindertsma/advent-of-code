9f3dbc34b34f08b577906c0e7dc298134e5892b2
Jens Meindertsma <mail@jensmeindertsma.com>
2022-12-22 16:16:57 +0100

    Set up parsing for day 12

--- years/2022/days/12/src/bin/part_a.rs
+++ years/2022/days/12/src/bin/part_a.rs
@@ -1,122 +1,27 @@
 #![feature(iter_next_chunk)]
 
-use std::{fmt, fs};
+use day_12::{Map, Pathfinder};
+use std::fs;
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
 
-    let pathfinder = Pathfinder::from(input.as_str());
+    let map = Map::from(input.as_str());
+    let pathfinder = Pathfinder::from(&map);
 
     let steps = pathfinder.run();
 
     println!("Minimum amount of steps required: {steps}");
 }
 
-struct Pathfinder {
-    map: Vec<Vec<Square>>,
-    position: (usize, usize),
-    elevation: usize,
-    steps: usize,
-}
-
-impl From<&str> for Pathfinder {
-    fn from(value: &str) -> Self {
-        let map: Vec<Vec<Square>> = value
-            .lines()
-            .map(|line| line.chars().map(Square::from).collect())
-            .collect();
-
-        let position = map
-            .iter()
-            .enumerate()
-            .find_map(|(y, row)| {
-                row.iter()
-                    .position(|square| *square == Square::Position)
-                    .map(|x| (x, y))
-            })
-            .unwrap();
-
-        Self {
-            map,
-            position,
-            elevation: 0,
-            steps: 0,
-        }
-    }
-}
-
-impl fmt::Debug for Pathfinder {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "\n--- MAP ---")
-            .and_then(|()| {
-                writeln!(
-                    f,
-                    "{}",
-                    self.map
-                        .iter()
-                        .map(|row| format!(
-                            "{}\n",
-                            row.iter()
-                                .map(|square| format!("{:<4}", format!("{square:?},")))
-                                .collect::<String>()
-                        ))
-                        .collect::<String>()
-                        .trim_end()
-                )
-            })
-            .and_then(|()| writeln!(f, "--- INTERNAL ---"))
-            .and_then(|()| writeln!(f, "Position: {:?}", self.position))
-            .and_then(|()| writeln!(f, "Elevation: {:?}", self.elevation))
-            .and_then(|()| writeln!(f, "Steps: {:?}", self.steps))
-    }
-}
-
-impl Pathfinder {
-    fn run(self) -> usize {
-        todo!("{:?}", self)
-    }
-}
-
-#[derive(PartialEq)]
-enum Square {
-    Regular(usize),
-    Position,
-    Destination,
-}
-
-impl fmt::Debug for Square {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Self::Destination => "E".to_owned(),
-                Self::Position => "S".to_owned(),
-                Self::Regular(height) => height.to_string(),
-            }
-        )
-    }
-}
-
-impl From<char> for Square {
-    fn from(value: char) -> Self {
-        match value {
-            'S' => Self::Position,
-            'E' => Self::Destination,
-            other => Self::Regular((other as usize) - 97),
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use day_12::TEST_INPUT;
-
-    use super::Pathfinder;
+    use day_12::{Map, Pathfinder, TEST_INPUT};
 
     #[test]
     fn test_input() {
-        let pathfinder = Pathfinder::from(TEST_INPUT);
+        let map = Map::from(TEST_INPUT);
+        let pathfinder = Pathfinder::from(&map);
 
         assert_eq!(pathfinder.run(), 31)
     }
--- years/2022/days/12/src/lib.rs
+++ years/2022/days/12/src/lib.rs
@@ -4,3 +4,84 @@ accszExk
 acctuvwj
 abdefghi
 ";
+
+pub struct Map {
+    grid: Vec<Vec<Position>>,
+}
+
+impl Map {
+    fn start(&self) -> (usize, usize) {
+        self.grid
+            .iter()
+            .enumerate()
+            .find_map(|(y, row)| {
+                row.iter()
+                    .position(|position| position == &Position::Start)
+                    .map(|x| (x, y))
+            })
+            .unwrap()
+    }
+
+    fn destination(&self) -> (usize, usize) {
+        self.grid
+            .iter()
+            .enumerate()
+            .find_map(|(y, row)| {
+                row.iter()
+                    .position(|position| position == &Position::Destination)
+                    .map(|x| (x, y))
+            })
+            .unwrap()
+    }
+}
+
+impl From<&str> for Map {
+    fn from(value: &str) -> Self {
+        let grid: Vec<Vec<Position>> = value
+            .lines()
+            .map(|line| line.chars().map(Position::from).collect())
+            .collect();
+
+        Self { grid }
+    }
+}
+
+#[derive(PartialEq)]
+enum Position {
+    Regular { height: u8 },
+    Start,
+    Destination,
+}
+
+impl From<char> for Position {
+    fn from(character: char) -> Self {
+        match character {
+            'S' => Self::Start,
+            'E' => Self::Destination,
+            other => Self::Regular {
+                height: other as u8,
+            },
+        }
+    }
+}
+
+pub struct Pathfinder<'a> {
+    map: &'a Map,
+}
+
+impl<'a> Pathfinder<'a> {
+    pub fn run(self) -> usize {
+        let start = self.map.start();
+        let destination = self.map.destination();
+
+        println!("Pathfinding from {start:?} to {destination:?}");
+
+        todo!()
+    }
+}
+
+impl<'a> From<&'a Map> for Pathfinder<'a> {
+    fn from(map: &'a Map) -> Self {
+        Self { map }
+    }
+}
