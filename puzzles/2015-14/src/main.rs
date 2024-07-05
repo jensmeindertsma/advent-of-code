00394df0e1bc2d89134c953a36c52578555c4c56
Jens Meindertsma <mail@jensmeindertsma.com>
2024-07-05 09:11:09 +0200

    day 14

--- puzzles/2015-14/src/main.rs
+++ puzzles/2015-14/src/main.rs
@@ -1,5 +1,5 @@
 use advent_of_code::{Answer, Puzzle};
-use puzzle_2015_14::part_1;
+use puzzle_2015_14::{part_1, part_2};
 
 fn main() {
     Puzzle {
@@ -7,11 +7,17 @@ fn main() {
         year: 2015,
         day: 14,
         part_1: |input: &str| {
-            Answer::new(part_1(input), |answer| {
+            Answer::new(part_1(input, 2503), |answer| {
                 format!("the winning reindeer traveled a distance of {answer}")
             })
         },
-        part_2: |_input: &str| None,
+        part_2: |input: &str| {
+            Some({
+                Answer::new(part_2(input, 2503), |answer| {
+                    format!("the winning reindeer scored {answer} points")
+                })
+            })
+        },
     }
     .solve(include_str!("../input.txt"))
 }
