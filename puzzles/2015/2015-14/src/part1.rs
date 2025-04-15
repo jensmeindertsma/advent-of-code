9ae577972f1944d223a70291158e63ab3266287a
Jens Meindertsma <mail@jensmeindertsma.com>
2025-04-15 09:25:38 +0200

    Edit todo

--- puzzles/2015/2015-14/src/part1.rs
+++ puzzles/2015/2015-14/src/part1.rs
@@ -1,5 +1,5 @@
 use std::time::Duration;
 
-pub fn part_1(input: &str, duration: Duration) -> usize {
-    input.len() + 42
+pub fn part_1(_input: &str, _duration: Duration) -> usize {
+    todo!("imlement 2015-14 part 1")
 }
