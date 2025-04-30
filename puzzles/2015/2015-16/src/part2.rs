4c6502498ebe50d84f8f14e9e119a63d6fd98616
Jens Meindertsma <mail@jensmeindertsma.com>
2025-04-30 20:31:11 +0200

    Refactor 2015-16 (#12)

--- puzzles/2015/2015-16/src/part2.rs
+++ puzzles/2015/2015-16/src/part2.rs
@@ -1,83 +1,44 @@
-use crate::sue::parse_sue;
+use crate::{
+    evaluator::{Evaluator, Target},
+    sue::parse_sue,
+};
 
 pub fn part_2(input: &str) -> u16 {
-    let target_akitas = 0;
-    let target_cars = 2;
-    let target_cats = 7;
-    let target_children = 3;
-    let target_goldfish = 5;
-    let target_perfumes = 1;
-    let target_pomeranians = 3;
-    let target_samoyeds = 2;
-    let target_trees = 3;
-    let target_vizslas = 0;
+    let target = Target {
+        akitas: 0,
+        cars: 2,
+        cats: 7,
+        children: 3,
+        goldfish: 5,
+        perfumes: 1,
+        pomeranians: 3,
+        samoyeds: 2,
+        trees: 3,
+        vizslas: 0,
+    };
 
-    input
+    let evaluator = Evaluator {
+        akitas: |value, target| value == target,
+        cars: |value, target| value == target,
+        cats: |value, target| value > target,
+        children: |value, target| value == target,
+        goldfish: |value, target| value < target,
+        perfumes: |value, target| value == target,
+        pomeranians: |value, target| value < target,
+        samoyeds: |value, target| value == target,
+        trees: |value, target| value > target,
+        vizslas: |value, target| value == target,
+    };
+
+    for sue in input
         .trim()
         .lines()
         .map(|line| parse_sue(line.trim()).unwrap())
-        .find_map(|sue| {
-            if let Some(akitas) = sue.akitas
-                && akitas != target_akitas
-            {
-                return None;
-            }
+    {
+        if evaluator.evaluate(&sue, &target) {
+            return sue.number;
+        }
+    }
 
-            if let Some(cars) = sue.cars
-                && cars != target_cars
-            {
-                return None;
-            }
-
-            if let Some(cats) = sue.cats
-                && cats <= target_cats
-            {
-                return None;
-            }
-
-            if let Some(children) = sue.children
-                && children != target_children
-            {
-                return None;
-            }
-
-            if let Some(goldfish) = sue.goldfish
-                && goldfish >= target_goldfish
-            {
-                return None;
-            }
-
-            if let Some(perfumes) = sue.perfumes
-                && perfumes != target_perfumes
-            {
-                return None;
-            }
-
-            if let Some(pomeranians) = sue.pomeranians
-                && pomeranians >= target_pomeranians
-            {
-                return None;
-            }
-
-            if let Some(samoyeds) = sue.samoyeds
-                && samoyeds != target_samoyeds
-            {
-                return None;
-            }
-
-            if let Some(trees) = sue.trees
-                && trees <= target_trees
-            {
-                return None;
-            }
-
-            if let Some(vizslas) = sue.vizslas
-                && vizslas != target_vizslas
-            {
-                return None;
-            }
-
-            Some(sue.number)
-        })
-        .unwrap()
+    panic!("No matching Sue was found!");
 }
