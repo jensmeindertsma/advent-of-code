131a35d309a6142f00ab1b56ce386851d0b81767
Jens Meindertsma <mail@jensmeindertsma.com>
2025-07-04 15:31:01 +0200

    2015-01

--- puzzles/2015/2015-01/src/part_one.rs
+++ puzzles/2015/2015-01/src/part_one.rs
@@ -1,3 +1,10 @@
 pub fn part_one(input: &str) -> isize {
-    input.len().try_into().unwrap()
+    input
+        .trim()
+        .chars()
+        .fold(0, |floor, character| match character {
+            '(' => floor + 1,
+            ')' => floor - 1,
+            other => panic!("Unexpected character `{other}`"),
+        })
 }
