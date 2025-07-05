a9dec6c8e3c5ccd854a24ee8cf743f94d3d43305
Jens Meindertsma <mail@jensmeindertsma.com>
2025-07-05 10:55:54 +0200

    Basic reporting

--- crates/spruce/src/lib.rs
+++ crates/spruce/src/lib.rs
@@ -1,1 +1,32 @@
-// TODO: beautiful puzzle output
+use std::{fmt::Display, time::Instant};
+
+pub struct Puzzle<One: Display, Two: Display> {
+    pub part_one: fn(&str) -> One,
+    pub part_two: Option<fn(&str) -> Two>,
+}
+
+impl<One: Display, Two: Display> Puzzle<One, Two> {
+    pub fn solve(&self, input: &str) {
+        let now = Instant::now();
+        let solution_one = (self.part_one)(input);
+        let duration = now.elapsed();
+
+        println!(
+            "part 1 = `{solution_one}` (took {}ms)",
+            duration.as_millis()
+        );
+
+        if let Some(part_two) = self.part_two {
+            let now = Instant::now();
+            let solution_one = part_two(input);
+            let duration = now.elapsed();
+
+            println!(
+                "part 2 = `{solution_one}` (took {}ms)",
+                duration.as_millis()
+            )
+        } else {
+            println!("part 2 unimplemented")
+        }
+    }
+}
--- puzzles/2015/2015-01/src/main.rs
+++ puzzles/2015/2015-01/src/main.rs
@@ -1,8 +1,12 @@
 use puzzle_2015_01::{part_one, part_two};
+use spruce::Puzzle;
 
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    println!("part 1: {}", part_one(INPUT));
-    println!("part 2: {}", part_two(INPUT))
+    Puzzle {
+        part_one,
+        part_two: Some(part_two),
+    }
+    .solve(INPUT);
 }
