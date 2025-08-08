cc2a33e52f9cd253c346ee742f49983123cd4160
Jens Meindertsma <mail@jensmeindertsma.com>
2025-08-08 17:10:43 +0200

    Better docs

--- puzzles/2015/2015-04/src/part_one.rs
+++ puzzles/2015/2015-04/src/part_one.rs
@@ -17,6 +17,15 @@ pub fn part_one(input: &str) -> usize {
 
             buffer.clear();
 
+            // One hexadecimal byte contains two characters.
+            // With indexding we check one byte at a time.
+            // With `result[0]` == 0 we are checking both digit 1 and digit 2.
+            // then for `result[1]`, we check digit 3 and 4.
+            //
+            // Then we us bitwise AND to look only at the first "digit" (4 bits) of the
+            // 5th byte. The bitwise operator will set the second digit in this byte to
+            // zero, so whether or not the `== 0` check passes depends only on the first
+            // 4 bits in the byte. If they are all zero (hexadecimal zero) then the check passed.
             result[0] == 0 && result[1] == 0 && (result[2] & 0xF0) == 0
         })
         .unwrap()
