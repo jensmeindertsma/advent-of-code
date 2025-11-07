319ab3e148e1b854b485bf79cee34b274f9d45cf
Jens Meindertsma <mail@jensmeindertsma.com>
2025-11-07 18:01:30 +0100

    Nom makes a return (#18)

--- puzzles/2015/2015-02/src/part_one.rs
+++ puzzles/2015/2015-02/src/part_one.rs
@@ -1,17 +1,28 @@
-use crate::parsing;
+use crate::parsing::parse;
 
 pub fn part_one(input: &str) -> usize {
     input
         .trim()
         .lines()
-        .map(|line| parsing::present(line).expect("parsing should succeed"))
-        .map(|present| {
-            let sides = present.sides().map(|s| s.area());
-            sides.iter().sum::<usize>()
-                + sides
-                    .iter()
-                    .min()
-                    .expect("one of the sides should be the smallest")
+        .map(|line| {
+            let cuboid = parse(line);
+
+            let total_surface_area = {
+                let l = cuboid.length;
+                let w = cuboid.width;
+                let h = cuboid.height;
+
+                (2 * l * w) + (2 * w * h) + (2 * h * l)
+            };
+
+            let smallest_area = cuboid
+                .faces()
+                .map(|(length, width)| length * width)
+                .into_iter()
+                .min()
+                .expect("one of the sides should be the smallest");
+
+            total_surface_area + smallest_area
         })
         .sum()
 }
