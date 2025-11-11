35a6111f500769da7b615e457452ffa5f6e850a0
Jens Meindertsma <mail@jensmeindertsma.com>
2025-11-11 21:08:08 +0100

    Fix implementation and tests of 2018-01

--- puzzles/2018/2018-01/src/lib.rs
+++ puzzles/2018/2018-01/src/lib.rs
@@ -16,7 +16,11 @@ fn one() {
 
 #[test]
 fn two() {
-    // assert_eq!(part_two("helloworld"), 0);
+    assert_eq!(part_two("+1, -2, +3, +1"), 2);
+    assert_eq!(part_two("+1, -1"), 0);
+    assert_eq!(part_two("+3, +3, +4, -2, -4"), 10);
+    assert_eq!(part_two("-6, +3, +8, +5, -6"), 5);
+    assert_eq!(part_two("+7, +7, -2, -7, -4"), 14);
 
-    // assert_eq!(part_two(include_str!("../input.txt")), 6);
+    assert_eq!(part_two(include_str!("../input.txt")), 219);
 }
