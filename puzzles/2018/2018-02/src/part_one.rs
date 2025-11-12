d4d0fbf5a7e7c9ee30f759d650af951c39ec6ce1
Jens Meindertsma <mail@jensmeindertsma.com>
2025-11-12 09:39:45 +0100

    part 1

---
+++ puzzles/2018/2018-02/src/part_one.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+pub fn part_one(input: &str) -> usize {
+    let mut has_double = 0;
+    let mut has_triple = 0;
+
+    for line in input.trim().lines() {
+        let characters = line.chars();
+        let mut counter = HashMap::new();
+
+        for character in characters {
+            println!("{line} -- {character}");
+            *counter.entry(character).or_insert(0) += 1;
+        }
+
+        println!("{:?}", counter);
+
+        if counter.values().any(|v| *v == 2) {
+            has_double += 1;
+        }
+
+        if counter.values().any(|v| *v == 3) {
+            has_triple += 1;
+        }
+    }
+
+    has_double * has_triple
+}
