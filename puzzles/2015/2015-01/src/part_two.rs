5b6f767b8f36a3fc9f26f123c7d9be74cd110ec0
Jens Meindertsma <mail@jensmeindertsma.com>
2026-04-28 09:57:09 +0200

    Solve 2015-01

---
+++ puzzles/2015/2015-01/src/part_two.rs
@@ -0,0 +1,16 @@
+pub fn part_two(input: &str) -> usize {
+    input
+        .trim()
+        .chars()
+        .scan(0, |floor, c| {
+            match c {
+                '(' => *floor += 1,
+                ')' => *floor -= 1,
+                _ => panic!("unexpected character `{c}`"),
+            }
+            Some(*floor)
+        })
+        .position(|floor| floor < 0)
+        .unwrap()
+        + 1
+}
