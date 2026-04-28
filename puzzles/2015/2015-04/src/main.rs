2872eb254a4adb62ff193bf407fde84045187f15
Jens Meindertsma <mail@jensmeindertsma.com>
2026-04-28 19:59:34 +0200

    Solve part two

--- puzzles/2015/2015-04/src/main.rs
+++ puzzles/2015/2015-04/src/main.rs
@@ -1,4 +1,4 @@
-use puzzle_2015_04::part_one;
+use puzzle_2015_04::{part_one, part_two};
 use spruce::Puzzle;
 
 const INPUT: &str = include_str!("../input.txt");
@@ -10,5 +10,6 @@ fn main() {
         day: 4,
     }
     .part_one(part_one)
+    .part_two(part_two)
     .solve(INPUT)
 }
--- puzzles/2015/2015-04/src/part_two.rs
+++ puzzles/2015/2015-04/src/part_two.rs
@@ -1,7 +1,7 @@
 use md5::{Digest, Md5};
 use std::fmt::Write;
 
-pub fn part_one(input: &str) -> usize {
+pub fn part_two(input: &str) -> usize {
     let prefix = input.trim();
 
     let mut buffer = String::with_capacity(prefix.len() + 20);
@@ -15,8 +15,8 @@ pub fn part_one(input: &str) -> usize {
 
         let result = Md5::digest(buffer.as_bytes());
 
-        // Check if first 20 bits (5 hex digits) are zero
-        if result[0] == 0 && result[1] == 0 && (result[2] & 0xF0) == 0 {
+        // Check if first 24 bits (6 hex digits) are zero
+        if result[0] == 0 && result[1] == 0 && (result[2]) == 0 {
             return number;
         }
     }
