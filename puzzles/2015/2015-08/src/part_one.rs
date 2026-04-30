e0a11b911c18feb79582758f69f784d3ac560cdd
Jens Meindertsma <mail@jensmeindertsma.com>
2026-04-30 19:12:52 +0200

    Improve 2015-08 part 1

--- puzzles/2015/2015-08/src/part_one.rs
+++ puzzles/2015/2015-08/src/part_one.rs
@@ -1,49 +1,48 @@
 pub fn part_one(input: &str) -> usize {
-    input
-        .trim()
-        .lines()
-        .map(|line| {
-            let literal_size = line.len();
+    let mut total = 0;
 
-            let mut memory_size = 0;
-            let mut characters = line.chars();
+    for line in input.trim().lines() {
+        let bytes = line.as_bytes();
+        let mut i = 0;
+        let mut memory = 0;
 
-            while let Some(character) = characters.next() {
-                match character {
-                    c if c.is_alphabetic() => memory_size += 1,
+        while i < bytes.len() {
+            match bytes[i] {
+                b'"' => {
+                    // ignore start and end double quote
+                    i += 1;
+                }
 
-                    '"' => continue,
+                b'\\' => match bytes[i + 1] {
+                    b'\\' | b'"' => {
+                        // backslash escape and double quote escape
+                        // both take 1 byte in memory
+                        memory += 1;
 
-                    '\\' => {
-                        let next = characters.next().unwrap();
-
-                        match next {
-                            '"' => {
-                                // `\"` is one memory space
-                                memory_size += 1
-                            }
-
-                            'x' => {
-                                // `\x??`` hexadecimal escape code for one character
-                                characters.next().unwrap();
-                                characters.next().unwrap();
-                                memory_size += 1
-                            }
-
-                            '\\' => {
-                                // `\\` double backslash is one backslash
-                                memory_size += 1
-                            }
-
-                            other => panic!("unhandled character `{other}`"),
-                        }
+                        // we parsed 2 bytes so move ahead in memory
+                        i += 2;
                     }
 
-                    other => panic!("unhandled character `{other}`"),
+                    b'x' => {
+                        // hexadecimal escape is 1 byte in memory
+                        memory += 1;
+
+                        // hexadecimal escape is 4 bytes in literal
+                        i += 4;
+                    }
+
+                    _ => unreachable!("invalid escape"),
+                },
+
+                _ => {
+                    memory += 1;
+                    i += 1;
                 }
             }
+        }
 
-            literal_size - memory_size
-        })
-        .sum()
+        total += line.len() - memory;
+    }
+
+    total
 }
