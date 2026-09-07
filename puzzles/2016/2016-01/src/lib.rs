58316e38b03399d5ad0bef0ba47013ec7f594b08
Jens Meindertsma <mail@jensmeindertsma.com>
2026-09-07 10:10:51 +0200

    Solve part 1

--- puzzles/2016/2016-01/src/lib.rs
+++ puzzles/2016/2016-01/src/lib.rs
@@ -6,7 +6,7 @@ pub use part_one::part_one;
 fn one() {
     assert_eq!(part_one("R2, L3"), 5);
     assert_eq!(part_one("R2, R2, R2"), 2);
-    assert_eq!(part_one("R5, L5, R5"), 12);
+    assert_eq!(part_one("R5, L5, R5, R3"), 12);
 
     assert_eq!(part_one(include_str!("../input.txt")), 273);
 }
--- puzzles/2016/2016-01/src/part_one.rs
+++ puzzles/2016/2016-01/src/part_one.rs
@@ -1,3 +1,67 @@
 pub fn part_one(input: &str) -> usize {
-    input.len()
+    let (_, (x, y)) = input
+        .trim()
+        .split(", ")
+        .map(|instruction| {
+            let (direction, distance) = instruction.split_at(1);
+
+            (
+                match direction {
+                    "L" => Direction::Left,
+                    "R" => Direction::Right,
+                    _ => panic!(),
+                },
+                distance.parse::<u8>().unwrap(),
+            )
+        })
+        .fold(
+            (Heading::North, (0i32, 0i32)),
+            |(heading, (mut x, mut y)), (direction, distance)| {
+                let new_heading = heading.turn(direction);
+
+                match new_heading {
+                    Heading::North => y += distance as i32,
+                    Heading::East => x += distance as i32,
+                    Heading::South => y -= distance as i32,
+                    Heading::West => x -= distance as i32,
+                }
+
+                (new_heading, (x, y))
+            },
+        );
+
+    (x.abs() + y.abs()) as usize
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Heading {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl Heading {
+    fn turn(self, direction: Direction) -> Self {
+        match direction {
+            Direction::Left => match self {
+                Heading::North => Heading::West,
+                Heading::West => Heading::South,
+                Heading::South => Heading::East,
+                Heading::East => Heading::North,
+            },
+            Direction::Right => match self {
+                Heading::North => Heading::East,
+                Heading::East => Heading::South,
+                Heading::South => Heading::West,
+                Heading::West => Heading::North,
+            },
+        }
+    }
 }
